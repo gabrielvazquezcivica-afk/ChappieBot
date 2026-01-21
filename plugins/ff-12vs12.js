b1c199b249a5a6cff9bbeefd3e4030e2beced88d
GABO <gabrielvazquezcivica@gmail.com>
2026-01-21 13:37:06 -0600

    Create ff-12vs12.js

---
+++ plugins/ff-12vs12.js
@@ -0,0 +1,65 @@
+export const handler = async (m, {
+  sock,
+  from,
+  isGroup,
+  isAdmin,
+  reply
+}) => {
+
+  if (!isGroup) {
+    return reply('❌ Este comando solo funciona en grupos')
+  }
+
+  // 🔒 Solo administradores
+  if (!isAdmin) {
+    return reply('🚫 Solo los *administradores* pueden crear la lista')
+  }
+
+  // 🥷 Reacción
+  await sock.sendMessage(from, {
+    react: { text: '🥷', key: m.key }
+  })
+
+  const text = `
+╭━━━━〔 🥷 12 VS 12 〕━━━━╮
+
+👑
+🥷 —
+🥷 —
+🥷 —
+───────
+
+👑
+🥷 —
+🥷 —
+🥷 —
+───────
+
+👑
+🥷 —
+🥷 —
+🥷 —
+───────
+
+🪑 *SUPLENTES*
+🥷 —
+🥷 —
+🥷 —
+🥷 —
+🥷 —
+🥷 —
+
+╰━━━━〔 🤖 ChappieBot 〕━━━━╯
+`.trim()
+
+  await sock.sendMessage(from, { text }, { quoted: m })
+}
+
+handler.command = ['12vs12']
+handler.tags = ['juegos']
+handler.help = ['12vs12']
+handler.menu = true
+handler.group = true
+handler.admin = true
+
+export default handler
