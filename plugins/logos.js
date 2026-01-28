bf048a988696dc1c92852fbec1177f4361122c0b
GABO <gabrielvazquezcivica@gmail.com>
2026-01-27 21:23:01 -0600

    Update logos.js

--- plugins/logos.js
+++ plugins/logos.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import axios from 'axios'
-import cheerio from 'cheerio'
+import * as cheerio from 'cheerio'
 
 /* ───── FUNCIÓN PARA DESCARGAR IMAGEN ───── */
 async function downloadImage(url) {
