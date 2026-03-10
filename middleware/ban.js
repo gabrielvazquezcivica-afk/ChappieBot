e3bf8afc0f5d7b82c4f3a7a865d4e1701ba883cb
GABO <gabrielvazquezcivica@gmail.com>
2026-03-10 03:07:05 -0600

    Refactor isBanned function to use global banList
    Refactor isBanned function to improve efficiency and readability.

--- middleware/ban.js
+++ middleware/ban.js
@@ -3,19 +3,12 @@ import path from 'path'
 
 const banPath = path.join('./data/ban.json')
 
-export const isBanned = (jid = '') => {
-
-  const number = jid.replace(/[^0-9]/g, '')
-
-  if (!fs.existsSync(banPath)) return false
-
+export const isBanned = (jid) => {
+  // Siempre leer la lista más reciente del archivo o usar la global
   let banList = {}
+  if (global.banList) banList = global.banList
+  else if (fs.existsSync(banPath)) banList = JSON.parse(fs.readFileSync(banPath))
 
-  try {
-    banList = JSON.parse(fs.readFileSync(banPath))
-  } catch {
-    banList = {}
-  }
-
-  return !!banList[number]
+  const normalized = jid.includes('@') ? jid : (jid.length > 15 ? jid+'@lid' : jid+'@s.whatsapp.net')
+  return !!banList[normalized]
 }
