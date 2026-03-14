1c7adf5ee9254499c06c69edc1307c10a1f8bab5
GABO <gabrielvazquezcivica@gmail.com>
2026-03-14 16:18:12 -0600

    Update ban.js

--- middleware/ban.js
+++ middleware/ban.js
@@ -4,11 +4,17 @@ const file = './data/ban.json'
 
 export function isBanned(user) {
 
+  if (!user) return false
   if (!fs.existsSync(file)) return false
 
   try {
+
     const banned = JSON.parse(fs.readFileSync(file))
-    return banned.includes(user)
+
+    const cleanUser = user.split(':')[0]
+
+    return banned.includes(cleanUser)
+
   } catch {
     return false
   }
