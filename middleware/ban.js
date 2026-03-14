f007d397a632cccae9ec30c52e6ba154591540e6
GABO <gabrielvazquezcivica@gmail.com>
2026-03-14 16:30:19 -0600

    Update ban.js

--- middleware/ban.js
+++ middleware/ban.js
@@ -2,18 +2,18 @@ import fs from 'fs'
 
 const file = './data/ban.json'
 
-export function isBanned(user) {
+export function isBanned(jid) {
 
-  if (!user) return false
+  if (!jid) return false
   if (!fs.existsSync(file)) return false
 
   try {
 
     const banned = JSON.parse(fs.readFileSync(file))
 
-    const cleanUser = user.split(':')[0]
+    const number = jid.split('@')[0].split(':')[0]
 
-    return banned.includes(cleanUser)
+    return banned.includes(number)
 
   } catch {
     return false
