3ff8fe31d0714ce8fd1f272f64160537b1c17c74
GABO <gabrielvazquezcivica@gmail.com>
2026-03-18 09:59:17 -0600

    Update and rename on-of-audios.js to on-off-audios.js

--- plugins/on-off-audios.js
+++ plugins/on-off-audios.js
@@ -1,6 +1,6 @@
-let handler = async (m, { args }) => {
+let handler = async (m, { args, reply }) => {
 
-if (!global.db) return m.reply('❌ Base de datos no cargada')
+if (!global.db) return reply('❌ Base de datos no cargada')
 
 if (!global.db.data) global.db.data = {}
 if (!global.db.data.chats) global.db.data.chats = {}
@@ -9,17 +9,17 @@ let chat = global.db.data.chats[m.chat] || (global.db.data.chats[m.chat] = {})
 
 if (args[0] === 'on') {
   chat.audios = true
-  m.reply('✅ Audios activados')
+  reply('✅ Audios activados')
 } else if (args[0] === 'off') {
   chat.audios = false
-  m.reply('❌ Audios desactivados')
+  reply('❌ Audios desactivados')
 } else {
-  m.reply('⚠️ Usa:\n.audios on\n.audios off')
+  reply('⚠️ Usa:\n.audios on\n.audios off')
 }
 }
 
 handler.command = ['audios']
-handler.tags = ['on-of']
+handler.tags = ['on-off']
 handler.help = ['audios on', 'audios off']
 handler.menu = true
 
