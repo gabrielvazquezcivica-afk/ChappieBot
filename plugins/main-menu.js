062e4b977634500ebd551aadbfaed894645ee549
GABO <gabrielvazquezcivica@gmail.com>
2026-03-19 15:26:34 -0600

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,3 +1,20 @@
+const sistema = (titulo = 'CHAPPIE BOT') => ({
+  key: {
+    fromMe: false,
+    participant: '0@s.whatsapp.net',
+    remoteJid: 'status@broadcast'
+  },
+  message: {
+    orderMessage: {
+      itemCount: 1,
+      message: titulo,
+      footerText: 'ChappieBot',
+      surface: 2,
+      sellerJid: '0@s.whatsapp.net'
+    }
+  }
+})
+  
 export const handler = async (m, {                  
   sock,                  
   from,                  
