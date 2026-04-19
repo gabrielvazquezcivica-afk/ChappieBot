1a2ed87e26fcd51c3839e6d4dc42d610eef5e16e
GABO <gabrielvazquezcivica@gmail.com>
2026-04-19 12:38:33 -0600

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,10 +1,14 @@
 export const handler = async (m, { sock, from }) => {
+  // Guardamos el tiempo en que se recibió el mensaje
+  const tiempoInicio = Date.now()
+
+  // Enviamos el mensaje y calculamos el tiempo de respuesta
   await sock.sendMessage(from, {
-    text: 'pong 🏓\n\n> ChappieBot'
+    text: `pong 🏓\n\n⚡ Velocidad: ${Date.now() - tiempoInicio} ms\n\n> ChappieBot`
   })
 }
 
 handler.command = ['ping']
-handler.help = ['ping']
+handler.help = ['p']
 handler.tags = ['info']
 handler.menu = true
